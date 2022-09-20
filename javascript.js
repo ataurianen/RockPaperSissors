03838f943b45af30e8ce3daf1feea896b9047353
Andrew Taurianen <andrew.taurianen@gmail.com>
2022-09-19 23:18:19 -0400

    Finished playerSelection Function and tested. Also added semi colons.

--- javascript.js
+++ javascript.js
@@ -1,26 +1,43 @@
 function computerSelection () {
    // Needs to return a random choice of Rock, Paper, or Sissors 
-    const max = 3
-    const min = 1
-    let choice = Math.floor(Math.random() * (max - min + 1) + min)
+    const max = 3;
+    const min = 1;
+    let choice = Math.floor(Math.random() * (max - min + 1) + min);
 
     if (choice == 1) {
-        return "Rock"
+        return "Rock";
     }
 
     if (choice == 2) {
-        return "Paper"
+        return "Paper";
     }
 
     if (choice == 3) {
-        return "Sissors"
+        return "Sissors";
     }
 }
 
 function playerSelection () {
     // Needs to ask player for thier choice and return it
+    while (true) {
+        let input = prompt("Rock, Paper, or Sissors?");
+        selection = input.toLowerCase();
+        
+        if (selection == 'rock' || selection == 'paper' || selection == 'sissors') {
+            return selection;
+        }
+        else {
+            alert("Not a valid selection");
+        }
+    }
+    
+    
+
+    
 }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
   }
+
+  console.log(playerSelection())
